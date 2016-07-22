977fe90c072e4cc4bb925cea044a21aa78757147
Nguyen Canh Toan <canhtoannguyen60@gmail.com>
2016-07-22 15:49:09 +0700

    Updating R program for new data

--- analysis_interaction_user_OA.R
+++ analysis_interaction_user_OA.R
@@ -1,6 +1,8 @@
 #This file analysis the data from file interaction_user_oa and output to file analysis_interaction_user_OA  
-current_dir <- "D:/Work/Zalo projects/Relationship between no of sent messages vs User interaction/"
-file_inp <- paste(current_dir, "src/final_version_here.csv", sep ="")
+current_dir <- "D:/Work/Zalo projects/Relationship between no of sent messages vs User interaction/src/groupedOA_sample/"
+
+file_inp <- paste(current_dir, "data_final.csv", sep ="")
+
 tab <-  read.csv(file_inp)
 
 tri_mean <-  function(myvec){
@@ -9,27 +11,27 @@ tri_mean <-  function(myvec){
 }
 
 
-header <- c("delivered", "no_of_elements", "seen", "clicked", "percentage_seen", "percentage_clicked", "percentage_clicked_vs_seen", 
-            "msg_ms_nhon", "msg_ms_dung", "conv_ms_nhon", "conv_ms_dung", "action_download_sticker", "lbs", "age")
+header <- c("delivered", "no_of_users", "sum_seen", "mean_seen", "mean_clicked", "percentage_seen", "percentage_clicked", "percentage_clicked_vs_seen", 
+            "msg", "conv", "reply", "no_reply", "age")
 
-write(header, file = paste(current_dir,"output/analysis_final.csv", sep = ""), sep = ",", ncolumns =  length(header))
+write(header, file = paste(current_dir,"analysis_final.csv", sep = ""), sep = ",", ncolumns =  length(header))
 
 for (val in min(tab$delivered):max(tab$delivered) ){
+
     temp <- tab[tab$delivered == val,]
-    seen <- mean(temp$seen)
-    clicked <- mean(temp$clicked)
+    sum_seen <- sum(temp$seen)
+    mean_seen <- tri_mean(temp$seen)
+    mean_clicked <- tri_mean(temp$clicked)
     percentage_seen <- sum(temp$seen)/sum(temp$delivered) * 100
     percentage_clicked <- sum(temp$clicked)/sum(temp$delivered) * 100
     percentage_clicked_vs_seen <- sum(temp$clicked)/sum(temp$seen) * 100
-    msg_ms_nhon <- tri_mean(temp$msg.x)
-    msg_ms_dung <- tri_mean(temp$msg.y)
+    msg <- tri_mean(temp$msg)
     age <- tri_mean(temp$age)
-    conv_ms_nhon <- tri_mean(temp$conv)
-    conv_ms_dung <- tri_mean(temp$reply + temp$no.reply)
-    action_download_sticker <- tri_mean(temp$action_download_sticker)
-    lbs <- tri_mean(temp$lbs)
+    conv <- tri_mean(temp$conv)
+    reply <- tri_mean(temp$reply)
+    no_reply <- tri_mean(temp$no_reply)
     
-    result <- c(val, length(temp$lbs), seen, clicked, percentage_seen, percentage_clicked, percentage_clicked_vs_seen, 
-              msg_ms_nhon, msg_ms_dung, conv_ms_nhon, conv_ms_dung, action_download_sticker, lbs, age)
-    write(result, file = paste(current_dir,"output/analysis_final.csv", sep = ""), append =  TRUE, sep = ",", ncolumns =  length(result))
+    result <- c(val, length(temp$delivered), sum_seen, mean_seen, mean_clicked, percentage_seen, percentage_clicked, percentage_clicked_vs_seen, 
+              msg, conv, reply, no_reply, age)
+    write(result, file = paste(current_dir,"analysis_final.csv", sep = ""), append =  TRUE, sep = ",", ncolumns =  length(result))
 }
--- data_merging_final.R
+++ data_merging_final.R
@@ -1,14 +1,15 @@
-current_dir = "D:/Work/Zalo projects/Relationship between no of sent messages vs User interaction/src/"
-file_user_statistics = paste(current_dir, "user_statistics", sep = "")
-file_user_oa_interaction = paste(current_dir, "user_oa_interaction", sep = "")
+#This file merge the already-merged files into one single file for processings
+current_dir = "D:/Work/Zalo projects/Relationship between no of sent messages vs User interaction/src/groupedOA_sample/"
+file_correlation_delivered_interaction = paste(current_dir, "OA/correlation_delivered_interaction.csv", sep = "")
+file_user_profile = paste(current_dir, "profile/user_profile_chat.csv", sep = "")
 
-table_stat = read.delim(file = file_user_statistics, header = TRUE, row.names = NULL, sep = "")
-table_inter = read.delim(file = file_user_oa_interaction, header = TRUE, row.names = NULL, sep = "")
+table_cor = read.delim(file = file_correlation_delivered_interaction, header = TRUE, row.names = NULL, sep = ",")
+table_user_profile = read.delim(file = file_user_profile, header = TRUE, row.names = NULL, sep = ",")
 
-table <- merge(table_inter,table_stat,  by = c("userid") )
-table$age = table$age/7
-table$gender = table$gender/7
+table <- merge(table_user_profile,table_cor,  by = c("userid", "msg") )
 
 table$age[table$age <= 10] <- mean(table$age)
 table$age[table$age >= 60] <- mean(table$age)
 
+file_out = paste(current_dir, "data_final.csv", sep ="")
+write.table(table, file_out, sep = ",", row.names = FALSE)
