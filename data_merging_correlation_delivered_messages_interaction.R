fed9033902ae17f651c0322c5e8926a2c342adf2
Nguyen Canh Toan <canhtoannguyen60@gmail.com>
2016-07-26 11:04:58 +0700

    Fixing errors: Header = true -> false

--- data_merging_correlation_delivered_messages_interaction.R
+++ data_merging_correlation_delivered_messages_interaction.R
@@ -6,12 +6,12 @@ current_dir = "D:/Work/Zalo projects/Relationship between no of sent messages vs
 file_list = c("2016-07-08","2016-07-09","2016-07-10","2016-07-11","2016-07-12","2016-07-13","2016-07-14")
 
 file_inp = paste(current_dir, file_list[1], sep = "")
-table <- read.delim(file = file_inp, header = TRUE, row.names = NULL, sep ="")
+table <- read.delim(file = file_inp, header = FALSE, row.names = NULL, sep ="")
 colnames(table) <- c("userid", "delivered", "seen", "msg", "conv", "clicked")
 
 for (i in 2:length(file_list)) {
   file_inp = paste(current_dir, file_list[i], sep = "")
-  current_table <- read.delim(file = file_inp, header = TRUE, row.names = NULL, sep ="")
+  current_table <- read.delim(file = file_inp, header = FALSE, row.names = NULL, sep ="")
   colnames(current_table) <- c("userid", "delivered1", "seen1", "msg1", "conv1", "clicked1")
   
   table <- merge(table, current_table, by =  c("userid"), all.x = TRUE, all.y = TRUE)
@@ -27,4 +27,4 @@ for (i in 2:length(file_list)) {
   table <- table[c(reduction_columns)]
 }
 file_out = paste(current_dir, "correlation_delivered_interaction.csv", sep ="")
-write.table(table, file_out, sep = ",", col.names = FALSE)
+write.table(table, file_out, sep = ",", row.names = FALSE)
--- data_merging_users_profile.R
+++ data_merging_users_profile.R
@@ -6,7 +6,7 @@ current_dir = "D:/Work/Zalo projects/Relationship between no of sent messages vs
 file_list = c("2016-07-08","2016-07-09","2016-07-10","2016-07-11","2016-07-12","2016-07-13","2016-07-14")
 
 file_inp = paste(current_dir, file_list[1], sep = "")
-table <- read.delim(file = file_inp, header = TRUE, row.names = NULL, sep ="")
+table <- read.delim(file = file_inp, header = FALSE, row.names = NULL, sep ="")
 colnames(table) <- c("userid",	"reply",	"no_reply",	"msg",	"daychat",	"gender",	"age")
 reduction_columns <- c("userid", "gender", "age", "reply", "no_reply", "msg")
 table <- table[reduction_columns]
@@ -14,7 +14,7 @@ table <- table[reduction_columns]
 for (i in 2:length(file_list)) {
   
   file_inp = paste(current_dir, file_list[i], sep = "")
-  current_table <- read.delim(file = file_inp, header = TRUE, row.names = NULL, sep ="")
+  current_table <- read.delim(file = file_inp, header = FALSE, row.names = NULL, sep ="")
   colnames(current_table) <- c("userid",	"reply1",	"no_reply1",	"msg1",	"daychat",	"gender",	"age")
   reduction_columns <- c("userid", "gender", "age", "reply1", "no_reply1", "msg1")
   current_table <- current_table[reduction_columns]
